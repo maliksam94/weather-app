3e2678c8b17550550d60cd895f97e0ac431bd047
abdulmalik <maliksam94@gmail.com>
2026-07-09 12:29:34 +0800

    Remove all files from repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# sample-weather-app
-This is a sample project that I created to learn about Angular, .net10 WebApi and .NET Aspire. It is a simple web application that shows a city's weather details.
--- webApp/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { WeatherUi } from './weather/weather-ui/weather-ui';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'weather', pathMatch: 'full' },
-  { path: 'weather', component: WeatherUi },
-  { path: '**', redirectTo: 'weather' }
-];
